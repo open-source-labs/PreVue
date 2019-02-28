828dcb0988e17f5ab504d24d6cb9808481e9f202
Daniel Shu <shudaniel95@gmail.com>
2019-02-28 17:03:38 -0500

    sidebar finished, finished setting up structure closed #2

---
+++ src/components/SideNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Tree</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
