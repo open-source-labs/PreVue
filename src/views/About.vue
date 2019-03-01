8a7a437ad9cb601a859b0dc0f4eddd9c205e4382
Annette Lin <al261310@gmail.com>
2019-03-01 11:51:38 -0500

    recent changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,14 +6,10 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {};
-=======
 import SideNav from '../components/SideNav';
 export default {
   components: {
     SideNav
   }
 };
->>>>>>> b655ae78c6c6e85b123d49e35a509f01a1da80ac
 </script>
