c89b8a84df5a56c3141e6fce4e464339e5c471cf
Daniel Shu <shudaniel95@gmail.com>
2019-03-06 12:01:16 -0500

    started working on EditSideBar

--- src/store/types.js
+++ src/store/types.js
@@ -1,5 +1,6 @@
 //Mutations
 export const ADD_TO_COMPONENT_MAP = 'ADD_TO_COMPONENT_MAP';
+export const UPDATE_CHILDREN = 'UPDATE_CHILDREN';
 
 //Actions
 export const registerComponent = 'registerComponent';
