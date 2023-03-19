6e5ec228cf6d6a9d6ee66be988eaad1ccc3f09a0
Sean Flynn <110478697+seanflynn5@users.noreply.github.com>
2023-03-19 10:31:54 -0400

    Update NewProjectComponent.vue
    add logic for newProjectComponent

--- src/components/NewProjectComponent.vue
+++ src/components/NewProjectComponent.vue
@@ -1,35 +1,66 @@
-<!-- <template>
-  
-// Keep or delete???
-
-
-  <button @click="addProject">
-    <i class="fas fa-folder-plus fa-lg"></i>
-
+<template>
+  <!--resets state for a new project-->
+  <v-btn id="new-project-btn" @click="newProject">
     <br />
-    <span class="white--text">Add Project</span>
-  </button>
+    <span class="white--text">NEW PROJECT</span>
+  </v-btn>
 </template>
 
 <script>
-import { addProject } from '../store/types';
+import { mapActions } from 'vuex';
+import icons from '../store/state/icons';
+import htmlElementMap from '../store/state/htmlElementMap';
 
 export default {
   name: 'NewProjectComponent',
-  created() {
-    Mousetrap.bind(['command+n', 'ctrl+n'], () => {
-      this.addProject();
-    });
-  },
   methods: {
-    addProject() {
-      this.$store.dispatch(addProject, {
-        filename: 'untitled-' + this.$store.state.projectNumber,
-        lastSavedLocation: ''
-      });
+    ...mapActions(['replaceState']),
+    newProject() {
+      const validated = this.$store.state.loggedIn;
+      const state = {
+        icons,
+        htmlElementMap,
+        componentMap: {
+          App: {
+            componentName: 'App',
+            children: ['HomeView'],
+            htmlList: []
+          },
+          HomeView: {
+            componentName: 'HomeView',
+            children: [],
+            htmlList: []
+          }
+        },
+        routes: {
+          HomeView: []
+        },
+
+        componentNameInputValue: '',
+        activeRoute: 'HomeView',
+        activeComponent: '',
+        selectedElementList: [],
+        projectName: 'Untitled-1',
+        componentChildrenMultiselectValue: [],
+        modalOpen: false,
+        htmlElements: [],
+        saved: false,
+        loggedIn: validated,
+        rerenderKey: 0
+      };
+      this.replaceState(state);
     }
   }
 };
 </script>
 
-<style></style> -->
+<style>
+#new-project-btn {
+  font-weight: 700;
+  font-size: 14px;
+}
+
+.white--text {
+  font-size: 14px;
+}
+</style>
